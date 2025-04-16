0d09242243d53b3704a22fba135692c12adef607
ronaldo maksen <maksengh@gmail.com>
2025-04-16 02:40:52 +0200

    fix: use import syntax in next.config.mjs for Vercel compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,29 @@
-// next.config.js
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  });
-  
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    env: {
-      HASHED_PASSWORD: process.env.HASHED_PASSWORD,
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    images: {
-      unoptimized: true,
-    },
-    experimental: {
-      webpackBuildWorker: true,
-      parallelServerBuildTraces: true,
-      parallelServerCompiles: true,
-    },
-  };
-  
-  module.exports = withPWA(nextConfig);
-  
+import nextPWA from 'next-pwa.js';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    HASHED_PASSWORD: process.env.HASHED_PASSWORD,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
+};
+
+export default nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  ...nextConfig,
+});
